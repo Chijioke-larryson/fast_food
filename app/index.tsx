5fd59fb866c4f1e5d96b888b653de9d45f275577
SilentXBT <larrysongift@gmail.com>
2025-07-15 04:04:52 -0700

    Day 1: setting up react native libraries and requirement, working on the background

---
+++ app/index.tsx
@@ -0,0 +1,36 @@
+import "./global.css"
+import {FlatList, Image, Pressable, Text, View} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {offers} from "@/ constants";
+import {Fragment} from "react";
+
+export default function Index() {
+    return (
+        <SafeAreaView>
+            <FlatList data={offers}
+                      renderItem={({ item, index}) =>{
+                          return (
+                              <View>
+                                  <Pressable className= "offer-card" style={{backgroundColor: item.color}}>
+                                      {() =>(
+                                          <Fragment>
+                                              <View className={"h-full w-1/2"}>
+                                                  <Image source={item.image} className={"size-full"} resizeMode={"contain"} />
+
+                                              </View>
+                                          </Fragment>
+                                      )}
+                                  </Pressable>
+
+
+                              </View>
+                          )
+                      }}
+            />
+
+
+        </SafeAreaView>
+
+
+    );
+}
