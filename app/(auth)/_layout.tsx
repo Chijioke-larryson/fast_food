89a29665df98aa3699e87c7b3a6e0af51575c769
SilentXBT <larrysongift@gmail.com>
2025-07-21 16:47:54 -0700

    Resolved merge conflict in _layout.tsx for button and login functionalities

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,17 +1,39 @@
-import {View, Text, KeyboardAvoidingView, ScrollView, Platform, Dimensions, ImageBackground} from 'react-native'
+
+import {View, Text, KeyboardAvoidingView, ScrollView, Platform, Dimensions, ImageBackground, Image} from 'react-native'
 import React from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
 import {Slot} from "expo-router";
 import {images} from "@/ constants";
 
+
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
+
 export default function _Layout() {
     return (
         <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
             <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled" >
+
                 <view className="w-full relative" style={{height: Dimensions.get("screen").height /2.25}}>
                     <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode="stretch" />
 
                 </view>
+
+                <View className="w-full relative" style={{height: Dimensions.get("screen").height /2.25}}>
+                    <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode="stretch" />
+                    <Image source={images.logo} className="self-center size-48 absolute -bottom-16 z-10" />
+
+                </View>
+                <CustomInput
+                    placeholder="enter your email"
+                    value={' '}
+                    onChangeText={(text) =>{}}
+                    label="Email"
+                    keyboardType="email-address"
+
+                />
+                <CustomButton />
+
 
 
             </ScrollView>
