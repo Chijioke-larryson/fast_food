2c6a1ba0676503ac36e2c07d647b5b18743319f0
SilentXBT <larrysongift@gmail.com>
2025-07-21 12:52:04 -0700

    appwrite functionalities

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,15 +1,23 @@
-import {View, Text} from 'react-native'
+import {View, Text, KeyboardAvoidingView, ScrollView, Platform, Dimensions, ImageBackground} from 'react-native'
 import React from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
 import {Slot} from "expo-router";
+import {images} from "@/ constants";
 
 export default function _Layout() {
     return (
-        <SafeAreaView>
-            <Text>Auth Layout</Text>
+        <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
+            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled" >
+                <view className="w-full relative" style={{height: Dimensions.get("screen").height /2.25}}>
+                    <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode="stretch" />
+
+                </view>
+
+
+            </ScrollView>
             <Slot />
 
-        </SafeAreaView>
+        </KeyboardAvoidingView>
 
 
     )
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig ={
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: "com.jsm_fast_food" ,
+    databaseId: '687e9499003c799255a4',
+    userCollectionId: "687e94e2001abdba738f",
+}
